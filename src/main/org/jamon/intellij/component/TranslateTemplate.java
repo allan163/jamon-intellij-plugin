c510adc5f324fd23aa14e2bace9d9aa2dd23bded
ghulam.sarwar <ghulam.sarwar@gmail.com>
2011-10-30 00:14:03 +0000

    Issue 7: Extracted the code to get the jamon lib urls, soure and destination folders to Utils.getJamonConfig() method from TranslateTemplate.translate().

--- src/main/org/jamon/intellij/component/TranslateTemplate.java
+++ src/main/org/jamon/intellij/component/TranslateTemplate.java
@@ -5,13 +5,11 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
-import org.jamon.intellij.configuration.ConfigurationState;
-import org.jamon.intellij.configuration.ConfigurationUtils;
+import org.jamon.intellij.util.Utils;
 
 import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
-import java.net.URL;
 import java.net.URLClassLoader;
 
 /**
@@ -28,42 +26,28 @@ public class TranslateTemplate extends AbstractProjectComponent {
     }
 
     public void translateFile(VirtualFile file) {
-        ConfigurationState state = myProject.getComponent(ConfigurationState.class);
-
-        URL[] urls = ConfigurationUtils.getJamonUrls(state);
-
-        for (URL url : urls) {
-            if (url.getFile().isEmpty()) {
-                Messages.showMessageDialog(myProject,
-                        "It appears that the Jamon plugin has not been properly configured yet.",
-                        "Jamon Not Found", Messages.getErrorIcon());
-                return;
-            }
+        JamonConfig jamonConfig = Utils.getJamonConfig(myProject, file);
+        if (jamonConfig == null) {
+            return;
         }
 
-        File srcDir = new File(file.getParent().getPresentableUrl());
-        File destDir = new File(state.destDir);
-        destDir.mkdirs();
+        translate(file, jamonConfig);
+    }
 
-        if (state.destDir.isEmpty() || !destDir.exists() || !destDir.isDirectory()) {
-            Messages.showMessageDialog(myProject,
-                    "It appears that the Jamon plugin has not been properly configured yet.",
-                    "No Output Directory", Messages.getErrorIcon());
-                return;
-        }
-
-        URLClassLoader classLoader = URLClassLoader.newInstance(urls);
+    private void translate(VirtualFile file, JamonConfig jamonConfig) {
+        URLClassLoader classLoader = URLClassLoader.newInstance(jamonConfig.getJamonLibUrls());
         try {
             Class clazz = classLoader.loadClass(TEMPLATE_PROCESSOR_CLASS);
             Constructor c = clazz.getConstructor(File.class, File.class, ClassLoader.class);
-            Object processor = c.newInstance(destDir, srcDir, classLoader);
+            Object processor = c.newInstance(jamonConfig.getDestDir(), jamonConfig.getSrcDir(),
+                classLoader);
             Method generateSource = clazz.getMethod("generateSource", String.class);
             generateSource.invoke(processor, file.getName());
         } catch (Exception e) {
             LOGGER.error(e.getMessage(), e);
             Messages.showMessageDialog(myProject,
-                    "Unable to load jamon, please check your settings.",
-                    "Unable to Translate", Messages.getErrorIcon());
+                "Unable to load jamon, please check your settings.",
+                "Unable to Translate", Messages.getErrorIcon());
         }
     }
 }
---
+++ src/main/org/jamon/intellij/util/Utils.java
@@ -0,0 +1,41 @@
+package org.jamon.intellij.util;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jamon.intellij.component.JamonConfig;
+import org.jamon.intellij.configuration.ConfigurationState;
+import org.jamon.intellij.configuration.ConfigurationUtils;
+
+import java.io.File;
+import java.net.URL;
+
+public class Utils {
+    public static JamonConfig getJamonConfig(Project project, VirtualFile file) {
+        ConfigurationState state = project.getComponent(ConfigurationState.class);
+
+        URL[] jamonLibUrls = ConfigurationUtils.getJamonUrls(state);
+
+        for (URL url : jamonLibUrls) {
+            if (url.getFile().isEmpty()) {
+                Messages.showMessageDialog(project,
+                    "It appears that the Jamon plugin has not been properly configured yet.",
+                    "Jamon Not Found", Messages.getErrorIcon());
+                return null;
+            }
+        }
+
+        File srcDir = new File(file.getParent().getPresentableUrl());
+        File destDir = new File(state.destDir);
+        destDir.mkdirs();
+
+        if (state.destDir.isEmpty() || !destDir.exists() || !destDir.isDirectory()) {
+            Messages.showMessageDialog(project,
+                    "It appears that the Jamon plugin has not been properly configured yet.",
+                    "No Output Directory", Messages.getErrorIcon());
+            return null;
+        }
+
+        return new JamonConfig(jamonLibUrls, srcDir, destDir);
+    }
+}
