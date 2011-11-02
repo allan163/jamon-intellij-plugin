29f188a0a163aa4f8c6f0752c6db2150a3a4aa4a
castral01 <castral01@23c43e31-7ceb-45e9-633f-4521bbd956ad>
2011-11-02 05:17:12 +0000

    Issue 16: DestDir has been refactored into outputDirectories and now works as a ModuleComponent.

--- src/main/org/jamon/intellij/configuration/ConfigurationState.java
+++ src/main/org/jamon/intellij/configuration/ConfigurationState.java
@@ -3,7 +3,15 @@ package org.jamon.intellij.configuration;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.module.Module;
 import com.intellij.util.xmlb.XmlSerializerUtil;
+import com.intellij.util.xmlb.annotations.MapAnnotation;
+import com.intellij.util.xmlb.annotations.Property;
+import com.intellij.util.xmlb.annotations.Tag;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * User: Ryan Brignoni
@@ -12,10 +20,35 @@ import com.intellij.util.xmlb.XmlSerializerUtil;
  */
 @State(name = "JamonPlugin", storages = {@Storage(id = "other", file="$PROJECT_FILE$")})
 public class ConfigurationState implements PersistentStateComponent<ConfigurationState> {
+    public static final String DEFAULT_OUTPUT_FOLDER = "generated-src";
+
     public String jamonRuntimeJar = "";
     public String jamonApiJar = "";
     public String jamonProcessorJar = "";
-    public String destDir = "";
+
+    @Tag("outputDirectories")
+    private Map<String, String> outputDirectories = new HashMap<String, String>();
+
+    public String getOutputDirectory(Module module) {
+        if (module != null) {
+            return outputDirectories.get(module.getName());
+        }
+        return null;
+    }
+
+    @Property()
+    @MapAnnotation()
+    public Map<String, String> getOutputDirectories() {
+        return outputDirectories;
+    }
+
+    public void setOutputDirectories(Map<String, String> directories) {
+        outputDirectories = directories;
+    }
+
+    public void setOutputDirectory(Module module, String outputDir) {
+        outputDirectories.put(module.getName(), StringUtils.isEmpty(outputDir) ? DEFAULT_OUTPUT_FOLDER : outputDir);
+    }
 
     public ConfigurationState getState() {
         return this;
---
+++ src/main/org/jamon/intellij/ui/ModuleConfigurationTab.java
@@ -0,0 +1,73 @@
+package org.jamon.intellij.ui;
+
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import org.apache.commons.lang.StringUtils;
+import org.jamon.intellij.configuration.ConfigurationState;
+import org.jamon.intellij.resource.IconResource;
+import org.jetbrains.annotations.Nls;
+
+import javax.swing.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ryan Brignoni
+ * Date: 11/1/11
+ * Time: 8:31 PM
+ */
+public class ModuleConfigurationTab implements Configurable {
+    private final Project myProject;
+    private final Module myModule;
+
+    private JPanel rootComponent;
+    private TextFieldWithBrowseButton outputDirectory;
+
+    public ModuleConfigurationTab(Module module) {
+        myProject = module.getProject();
+        myModule = module;
+
+        outputDirectory.addBrowseFolderListener("Choose Output Destination",
+                "Select the directory where you would like to save the compiled jamon templates.",
+                myProject, FileChooserDescriptorFactory.createSingleFolderDescriptor());
+    }
+
+    @Nls
+    public String getDisplayName() {
+        return "Jamon";
+    }
+
+    public Icon getIcon() {
+        return IconResource.get().toolbarIcon();
+    }
+
+    public String getHelpTopic() {
+        return null;
+    }
+
+    public JComponent createComponent() {
+        return rootComponent;
+    }
+
+    public boolean isModified() {
+        ConfigurationState configuration = myProject.getComponent(ConfigurationState.class);
+        String output = outputDirectory.getText();
+        return (!StringUtils.isEmpty(output) && !output.equals(configuration.getOutputDirectory(myModule)));
+    }
+
+    public void apply() throws ConfigurationException {
+        ConfigurationState configuration = myProject.getComponent(ConfigurationState.class);
+        configuration.setOutputDirectory(myModule, outputDirectory.getText());
+    }
+
+    public void reset() {
+        ConfigurationState configuration = myProject.getComponent(ConfigurationState.class);
+        outputDirectory.setText(configuration.getOutputDirectory(myModule));
+    }
+
+    public void disposeUIResources() {
+    }
+}
