ae9330945193957e0a99a74b926751e362d4720a
ghulam.sarwar@gmail.com <ghulam.sarwar@gmail.com@23c43e31-7ceb-45e9-633f-4521bbd956ad>
2011-11-12 23:57:04 +0000

    Issue7 : Jump back to jamon also working now.

--- trunk/src/main/org/jamon/intellij/action/JumpToImplAction.java
+++ trunk/src/main/org/jamon/intellij/action/JumpToImplAction.java
@@ -1,15 +1,20 @@
 package org.jamon.intellij.action;
 
+import com.intellij.ide.util.PackageUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiJavaFile;
+import com.intellij.psi.PsiManager;
 import org.jamon.intellij.configuration.ConfigurationUtils;
 import org.jamon.intellij.configuration.JamonConfig;
 import org.jamon.intellij.resource.JamonIconProvider;
@@ -31,16 +36,16 @@ public class JumpToImplAction extends AnAction{
       if (project != null) {
           VirtualFile file = DataKeys.VIRTUAL_FILE.getData(dataContext);
         if (file != null) {
+            PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
             Module module = DataKeys.MODULE.getData(dataContext);
             ModuleRootManager manager = ModuleRootManager.getInstance(module);
             VirtualFile srcDir = Utils.getSourcePath(manager, file);
             JamonConfig jamonConfig = ConfigurationUtils.getJamonConfig(project, srcDir, file);
-            String filePath = getFilePathToOpen(file, jamonConfig);
+            String filePath = getFilePathToOpen(manager, psiFile, file, jamonConfig);
             openFile(project, filePath);
         }
       }
     }
-
 
     private void openFile(Project project, String filePath) {
         VirtualFile fileToOpen = LocalFileSystem.getInstance().findFileByPath(filePath);
@@ -50,22 +55,39 @@ public class JumpToImplAction extends AnAction{
         }
     }
 
-    private String getFilePathToOpen(VirtualFile file, JamonConfig jamonConfig) {
+    private String getFilePathToOpen(ModuleRootManager manager, PsiFile psiFile, VirtualFile file,
+                                     JamonConfig jamonConfig) {
         String extension = file.getExtension();
         if (extension != null) {
             if (extension.contains("jamon")) {
                 return getImplFileName(file, jamonConfig);
             }
-            else if (extension.contains(".java")) {
-                return getJamonFileName(file, jamonConfig);
+            else if (extension.contains("java")) {
+                return getJamonFileName(manager, psiFile, file, jamonConfig);
             }
         }
         return null;
     }
 
-    private String getJamonFileName(VirtualFile file, JamonConfig jamonConfig) {
-//        file.getParent().getPresentableUrl() + file.getNameWithoutExtension() + ".jamon";
-//        VirtualFile fileToOpen = LocalFileSystem.getInstance().findFileByPath();
+    private String getJamonFileName(ModuleRootManager manager, PsiFile psiFile, VirtualFile file, JamonConfig jamonConfig) {
+        if (psiFile instanceof PsiJavaFile) {
+            PsiJavaFile psiJavaFile = (PsiJavaFile) psiFile;
+            String packageName = psiJavaFile.getPackageName();
+            String packagePath = packageName.replace('.', File.separatorChar);
+            String jamonSourcePath = packagePath + File.separator + file.getNameWithoutExtension() + ".jamon";
+            return findInModuleSources(manager, jamonSourcePath);
+        }
+        return null;
+    }
+
+    private String findInModuleSources(ModuleRootManager manager, String relativePathFromSource) {
+        for (VirtualFile sourcePath : manager.getSourceRoots()) {
+            String pathToFind = sourcePath.getPresentableUrl() + File.separator + relativePathFromSource;
+            VirtualFile file = LocalFileSystem.getInstance().findFileByPath(pathToFind);
+            if (file != null) {
+                return file.getPresentableUrl();
+            }
+        }
         return null;
     }
 
