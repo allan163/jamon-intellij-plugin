66255016fa1f2df33ad22f7d8fc70651c80bc7eb
ghulam.sarwar@gmail.com <ghulam.sarwar@gmail.com@23c43e31-7ceb-45e9-633f-4521bbd956ad>
2011-11-12 22:04:11 +0000

    Issue7 :  Jump to implementation is done, we still have to do proxy and back to template.

--- trunk/src/main/org/jamon/intellij/util/Utils.java
+++ trunk/src/main/org/jamon/intellij/util/Utils.java
@@ -3,6 +3,7 @@ package org.jamon.intellij.util;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.components.ComponentManager;
+import com.intellij.openapi.roots.ModuleRootModel;
 import com.intellij.openapi.vfs.VirtualFile;
 
 public class Utils {
@@ -21,5 +22,17 @@ public class Utils {
         String relativePath = target.getPresentableUrl();
         relativePath = relativePath.replace(baseDir.getPresentableUrl(), "");
         return relativePath;
+    }
+
+    public static VirtualFile getSourcePath(ModuleRootModel manager, VirtualFile template) {
+        VirtualFile srcDir = null;
+        for (VirtualFile sourcePath : manager.getSourceRoots()) {
+            if (template != null
+                    && template.getPresentableUrl().contains(sourcePath.getPresentableUrl())) {
+                srcDir = sourcePath;
+                break;
+            }
+        }
+        return srcDir;
     }
 }
