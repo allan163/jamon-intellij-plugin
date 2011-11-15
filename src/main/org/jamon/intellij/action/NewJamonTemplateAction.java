41843d00b8e431e51440ee137d0a43c276905e47
castral01 <castral01@23c43e31-7ceb-45e9-633f-4521bbd956ad>
2011-11-15 07:29:50 +0000

    Issue 18: Simplifying New Jamon Template Action, getting rid of reliance on TemplateManager because it's a huge pain in the ass.

--- src/main/org/jamon/intellij/action/NewJamonTemplateAction.java
+++ src/main/org/jamon/intellij/action/NewJamonTemplateAction.java
@@ -1,13 +1,12 @@
 package org.jamon.intellij.action;
 
-import com.intellij.CommonBundle;
-import com.intellij.ide.actions.CreateFileFromTemplateDialog;
-import com.intellij.ide.actions.CreateTemplateInPackageAction;
+import com.intellij.ide.actions.CreateFileAction;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiElement;
-import com.intellij.util.IncorrectOperationException;
-import org.jamon.intellij.lang.file.JamonFile;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.*;
 import org.jamon.intellij.lang.file.JamonFileType;
 import org.jamon.intellij.resource.JamonIconProvider;
 import org.jetbrains.annotations.NotNull;
@@ -18,45 +17,64 @@ import org.jetbrains.annotations.NotNull;
  * Date: 11/3/11
  * Time: 8:51 PM
  */
-public class NewJamonTemplateAction extends CreateTemplateInPackageAction<JamonFile> {
+public class NewJamonTemplateAction extends CreateFileAction {
+    public static final String ACTION_NAME = "Create New Jamon Template";
 
     protected NewJamonTemplateAction() {
-        super("Jamon Template", "Creates new Jamon Template",
-                JamonIconProvider.JAMON_ICON_16, true);
-    }
-
-    @Override
-    protected PsiElement getNavigationElement(@NotNull JamonFile jamonFile) {
-        return jamonFile;
+        super(ACTION_NAME, ACTION_NAME, JamonIconProvider.JAMON_ICON_16);
     }
 
     @NotNull
     @Override
-    protected CreateFileFromTemplateDialog.Builder buildDialog(Project project,
-                                                               PsiDirectory psiDirectory) {
-        final CreateFileFromTemplateDialog.Builder builder = CreateFileFromTemplateDialog.
-                createDialog(project, "New Jamon Template");
-        builder.addKind("Template", JamonIconProvider.JAMON_ICON_16, "Jamon.jamon");
-        return builder;
+    protected PsiElement[] create(String newName, PsiDirectory directory) throws Exception {
+        Project project = directory.getProject();
+        PsiFile file = PsiFileFactory.getInstance(project).createFileFromText(
+                newName, JamonFileType.JAMON_FILE_TYPE, "");
+        return new PsiElement[]{directory.add(file)};
+    }
+
+    @NotNull
+    @Override
+    protected PsiElement[] invokeDialog(Project project, PsiDirectory directory) {
+        JamonValidator validator = new JamonValidator(project, directory);
+        Messages.showInputDialog(project, "Enter new Jamon Template Name",
+                "New Jamon Template", Messages.getQuestionIcon(), "template.jamon", validator);
+        return validator.getCreatedElements();
     }
 
     @Override
-    protected JamonFile doCreate(PsiDirectory directory, String templateName, String templateType)
-            throws IncorrectOperationException {
+    public boolean isAvailable(DataContext dataContext) {
+        Project project = PlatformDataKeys.PROJECT.getData(dataContext);
+        VirtualFile file = PlatformDataKeys.VIRTUAL_FILE.getData(dataContext);
 
-        final String fileName = templateName + "." + JamonFileType.DEFAULT_EXTENSION;
+        if (!super.isAvailable(dataContext)) {
+            return false;
+        }
 
-        return (JamonFile) JamonTemplateFactory
-                .createFromTemplate(directory, templateName, fileName, templateType);
+        if (file == null) {
+            return false;
+        }
+
+        if (!file.isDirectory() || !file.isWritable()) {
+            return false;
+        }
+
+        assert project != null;
+        PsiDirectory directory = PsiManager.getInstance(project).findDirectory(file);
+
+        return directory != null
+                && JavaDirectoryService.getInstance().getPackage(directory) != null;
+
     }
 
-    @Override
-    protected String getActionName(PsiDirectory psiDirectory, String s, String s1) {
-        return "Jamon Template";
-    }
+    private final class JamonValidator extends MyValidator {
+        public JamonValidator(Project project, PsiDirectory directory) {
+            super(project, directory);
+        }
 
-    @Override
-    protected String getErrorTitle() {
-        return CommonBundle.getErrorTitle();
+        @Override
+        public boolean checkInput(String inputString) {
+            return inputString.endsWith(JamonFileType.DEFAULT_EXTENSION);
+        }
     }
 }
